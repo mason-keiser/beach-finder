8410507bd014f62d7778e8702f4417702ed10d4d
Mason Keiser <masonksr5@gmail.com>
2020-08-15 02:49:32 +0000

    homepage styled

---
+++ client/components/homepage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class Homepage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    
+
+    render() {
+        return (
+            <div className='background'>
+                <header>Beach Finder</header>
+                    <div className='logo'>
+                        <img src="/images/skull-surfer.jpg" alt=""/>
+                    </div>
+                <div className='locationInput'>Enter your location ...</div>
+            </div>
+        )
+    }
+}

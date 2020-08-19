363f5bbfa8b585883952ada4db9f7b369e8d8c12
Mason Keiser <masonksr5@gmail.com>
2020-08-19 01:55:42 +0000

    Location Name passed through marker click to weather module

--- client/components/weather.jsx
+++ client/components/weather.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+const api = {
+    key: "6d7d1e7d4d37757e05e72f81a893fa86",
+    baseurl: "http://api.openweathermap.org/data/2.5/"
+}
+
 export default class Weather extends React.Component{
     constructor(props) {
         super(props);
@@ -9,6 +14,7 @@ export default class Weather extends React.Component{
     render() {
         return (
             <div className='background'>
+                <header>{this.props.markerInfo.params.props.name}</header>
                 <div className= 'homeButton' onClick={() => this.props.setView('home', {})}>Home</div>
             </div>
         )

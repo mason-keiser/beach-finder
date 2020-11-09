4d6b942f0ff461cf2f756b64717d3892d7592458
Mason Keiser <masonksr5@gmail.com>
2020-11-09 20:16:14 +0000

    fixed ipad responsiveness

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -25,7 +25,6 @@ export default class App extends React.Component {
       .then(data => this.setState({ message: data.message || data.error }))
       .catch(err => this.setState({ message: err.message }))
       .finally(() => this.setState({ isLoading: false }));
-      this.viewBeachInfo()
   }
 
   viewBeachInfo() {
